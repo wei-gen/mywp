296d7f86ac868c1c1a78c55f3a5b7c239bc8cfb2
luo <1193984561@qq.com>
2021-04-24 17:41:16 +0800

    研究源码文件分开放

--- RemoteInjector.h
+++ RemoteInjector.h
@@ -9,10 +9,12 @@ class RemoteInjector
 public:
 	//目标进程PID
 	DWORD targetProcPID = 0;
-	string strDllPath;
+	char cDllPath[250];
 	//获取进程句柄，提升权限
-	int EnableDebugPriv(const char* name);
+	int enableDebugPriv(const char* name);
 	//执行远程注入
 	int remoteInjection(const DWORD PID);
+	void setcDllPath(string path);
+	void setTargetProcPID(long pid);
 
 };
